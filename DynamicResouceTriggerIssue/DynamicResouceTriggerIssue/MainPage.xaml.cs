c7d2d1c315ee63590e7f9bc59a1649a71c2105d5
Chase Marsh <cmarsh@gebcservices.com>
2019-07-12 21:58:08 -0500

    Added DynamicResourceTriggerIssue

---
+++ DynamicResouceTriggerIssue/DynamicResouceTriggerIssue/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DynamicResouceTriggerIssue
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageVM();
+        }
+    }
+
+    public class MainPageVM : INotifyPropertyChanged
+    {
+        private bool _isDark;
+        private bool _isTriggered;
+
+        public bool IsDark
+        {
+            get => _isDark;
+            set
+            {
+                _isDark = value;
+                OnPropertyChanged(nameof(IsDark));
+
+                ThemeManager.CurrentTheme = _isDark ? Theme.Dark : Theme.Default;
+
+            }
+        }
+
+        public bool IsTriggered
+        {
+            get => _isTriggered;
+            set
+            {
+                _isTriggered = value;
+                OnPropertyChanged(nameof(IsTriggered));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DynamicResouceTriggerIssue/DynamicResouceTriggerIssue/ThemeManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace DynamicResouceTriggerIssue
+{
+    public enum Theme
+    {
+        Default,
+        Dark
+    }
+
+    public static class ThemeManager
+    {
+        private static Theme _currentTheme;
+        public static Theme CurrentTheme
+        {
+            get { return _currentTheme; }
+            set
+            {
+                _currentTheme = value;
+                ApplyTheme(_currentTheme);
+            }
+        }
+        private static void ApplyTheme(Theme theme)
+        {
+            if (Application.Current != null)
+            {
+                if (!Application.Current.Resources.MergedDictionaries.Any((rd) => rd is ColorResources))
+                    Application.Current.Resources.MergedDictionaries.Add(new ColorResources());
+
+                switch (CurrentTheme)
+                {
+                    case Theme.Default:
+                        UpdateResources<Color>(new ColorResources());
+                        break;
+                    case Theme.Dark:
+                        UpdateResources<Color>(new DarkColorResources());
+                        break;
+                }
+            }
+        }
+
+        private static void UpdateResources<T>(ResourceDictionary newResources)
+        {
+            foreach (var newResourceKey in newResources.Keys)
+            {
+                if (Application.Current.Resources.TryGetValue(newResourceKey, out var appResource))
+                {
+                    if (appResource is T)
+                    {
+                        Application.Current.Resources[newResourceKey] = newResources[newResourceKey];
+                    }
+                }
+            }
+        }
+    }
+}
